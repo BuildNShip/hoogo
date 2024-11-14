a8fa6d45c130a85fb5958416ea468a442b66ba3a
Aswin Asok <aswinasokofficial@gmail.com>
2024-11-14 20:33:36 +0530

    feat(login):ui-improvements

--- src/main.tsx
+++ src/main.tsx
@@ -7,22 +7,22 @@ import { Toaster, ToastPosition } from "react-hot-toast";
 import { Routes } from "./router";
 
 const toasterProps = {
-  containerStyle: {
-    fontFamily: "Nunito, sans-serif",
-  },
-  toastOptions: {
-    style: {
-      backgroundColor: "#1B2725",
-      border: "0.5px solid #232A2B",
-      color: "#fdfdfd",
+    containerStyle: {
+        fontFamily: "Nunito, sans-serif",
     },
-  },
-  position: "bottom-center" as ToastPosition,
+    toastOptions: {
+        style: {
+            backgroundColor: "#252525",
+            border: "0.5px solid #232A2B",
+            color: "#fdfdfd",
+        },
+    },
+    position: "bottom-center" as ToastPosition,
 };
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Routes />
-    <Toaster {...toasterProps} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Routes />
+        <Toaster {...toasterProps} />
+    </React.StrictMode>
 );
--- src/pages/User/Login/Login.tsx
+++ src/pages/User/Login/Login.tsx
@@ -5,43 +5,73 @@ import styles from "./Login.module.css";
 import { useParams } from "react-router-dom";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
-import Navbar from "../../../components/Navbar/Navbar";
 import Footer from "../../../components/Footer/Footer";
-const Login = () => {
-  const { eventName } = useParams();
-  const navigate = useNavigate();
-  const [ticketCode, setTicketCode] = useState<string>();
-  const onSubmit = () => {
-    if (ticketCode === undefined) {
-      toast.error("Please enter ticket code");
-    } else {
-      navigate("/" + eventName + "/" + ticketCode);
-    }
-  };
 
-  return (
-    <>
-      <Navbar />
-      <div className={styles.centerContainer}>
-        <div className={styles.container}>
-          <label className={styles.loginHeading}>
-            {eventName?.toUpperCase()}
-          </label>
-          <input
-            className={styles.loginInput}
-            type="text"
-            placeholder="Ticket Code"
-            value={ticketCode}
-            onChange={(e) => setTicketCode(e.target.value)}
-          />
-          <button className={styles.loginButton} onClick={() => onSubmit()}>
-            Login
-          </button>
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
+const Login = () => {
+    const { eventName } = useParams();
+    const navigate = useNavigate();
+    const [ticketCode, setTicketCode] = useState<string>();
+
+    const onSubmit = () => {
+        if (ticketCode === undefined) {
+            toast.error("Please enter ticket code", {
+                id: "ticketCode",
+            });
+        } else {
+            navigate("/" + eventName + "/" + ticketCode);
+        }
+    };
+
+    return (
+        <>
+            <div className={styles.mainContainer}>
+                <p className={styles.pageHeaderText}>
+                    Hi, <span>Elevate 2024</span> Participants 👋
+                </p>
+                <div className={styles.loginContainer}>
+                    <div className={styles.logoContainerHeader}>
+                        <p className={styles.loginContainerHeaderText}>Welcome To Hoogo</p>
+                        <p className={styles.loginContainerDescription}>
+                            Create, play, and make networking faster and fun.
+                        </p>
+                    </div>
+                    <div className={styles.inputFieldContainer}>
+                        <p className={styles.inputFieldLabel}>Ticket Code *</p>
+                        <p className={styles.inputFieldDescription}>
+                            Kindly refer your ticket for the code.
+                        </p>
+                        <input
+                            className={styles.inputField}
+                            type="text"
+                            placeholder="MMPXXXXXXXX"
+                            value={ticketCode}
+                            onChange={(e) => setTicketCode(e.target.value)}
+                        />
+                    </div>
+                    <button
+                        className={styles.loginButton}
+                        style={
+                            ticketCode && ticketCode?.length > 0
+                                ? { backgroundColor: "#ffd700", color: "#252525", opacity: 1 }
+                                : {
+                                      backgroundColor: "#252525",
+                                      cursor: "not-allowed",
+                                  }
+                        }
+                        onClick={onSubmit}
+                    >
+                        Login
+                    </button>
+                </div>
+
+                <p className={styles.funFact}>
+                    <span>Fun Fact:</span> The very first version of this game was built the day
+                    before its very first event.
+                </p>
+            </div>
+            <Footer />
+        </>
+    );
 };
 
 export default Login;

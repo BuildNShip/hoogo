f670f73c5729a09b8e39627fb5124f52aadefa30
Aswin Asok <aswinasokofficial@gmail.com>
2024-11-15 13:10:22 +0530

    feat(individal):leaderboard-ux-revamped

--- src/pages/Admin/BingoCard/BingoCard.tsx
+++ src/pages/Admin/BingoCard/BingoCard.tsx
@@ -2,53 +2,48 @@ import { useParams } from "react-router-dom";
 import styles from "./BingoCard.module.css";
 import { useEffect, useState } from "react";
 import { getBingoMatrix } from "../../../apis/common";
+import Footer from "../../../components/Footer/Footer";
 
 interface BingoItem {
-  name: string;
-  liner: string;
-  image: string;
+    name: string;
+    liner: string;
+    image: string;
 }
 
 const BingoCard = () => {
-  const { playerName, eventName } = useParams();
-  const dummyImageUrl = "/logoplace.png";
-  const [bingoAnswers, setBingoAnswers] = useState<BingoItem[][]>([]);
+    const { playerName, eventName } = useParams();
+    const dummyImageUrl = "/logoplace.png";
+    const [bingoAnswers, setBingoAnswers] = useState<BingoItem[][]>([]);
 
-  useEffect(() => {
-    getBingoMatrix(eventName, playerName).then((data) => {
-      setBingoAnswers(data.answer);
-    });
-  }, [eventName, playerName]);
+    useEffect(() => {
+        getBingoMatrix(eventName, playerName).then((data) => {
+            setBingoAnswers(data.answer);
+        });
+    }, [eventName, playerName]);
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.grid}>
-        {bingoAnswers.map((item, index) => (
-          <>
-            <div key={index} className={styles.row}>
-              {item.map((cell, cellIndex) => (
-                <div key={cellIndex} className={styles.cell}>
-                  <img
-                    src={cell.image ? cell.image : dummyImageUrl}
-                    alt={cell.name}
-                    className={styles.image}
-                  />
-                  {cell.name && cell.liner && (
-                    <div className={styles.overlay}>
-                      <p className={styles.name}>{cell.name.length}</p>
-                      <p className={styles.liner}>{cell.liner}</p>
-                    </div>
-                  )}
-                </div>
-              ))}
+    return (
+        <div className={styles.container}>
+            <div className={styles.grid}>
+                {bingoAnswers.map((item, index) => (
+                    <>
+                        <div key={index} className={styles.row}>
+                            {item.map((cell, cellIndex) => (
+                                <div key={cellIndex} className={styles.cell}>
+                                    <img
+                                        src={cell.image ? cell.image : dummyImageUrl}
+                                        alt={cell.name}
+                                        className={styles.image}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    </>
+                ))}
             </div>
-            <br />
-          </>
-        ))}
-      </div>
-      <h1 className={styles.title}>{playerName}'s BINGO Card</h1>
-    </div>
-  );
+            <Footer />
+            {/* <h1 className={styles.title}>{playerName}'s BINGO Card</h1> */}
+        </div>
+    );
 };
 
 export default BingoCard;

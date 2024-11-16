5a954230c4f2cb4abb9a5a0d2bf4e63180ea3799
Aswin <aswinasokofficial@gmail.com>
2024-11-16 10:32:34 +0530

    fix(build)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,8 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   return (
     <>
       <div className={styles.navbarContainer}>

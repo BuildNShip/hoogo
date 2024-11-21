f920087643d6e6a4922f12a4d203a06bf2d6e70f
Aswin Asok <aswinasokofficial@gmail.com>
2024-11-21 15:31:17 +0530

    feat(scale)

--- src/pages/Admin/BingoCard/functions.ts
+++ src/pages/Admin/BingoCard/functions.ts
@@ -22,7 +22,7 @@ export const captureAndDownload = async ({
                 useCORS: true,
                 allowTaint: false,
                 backgroundColor: null,
-                scale: 1,
+                scale: 2, // Increase scale for higher resolution
             });
 
             const gridImage = gridCanvas.toDataURL("image/png");
@@ -77,7 +77,7 @@ export const captureAndDownloadPost = async ({
                 useCORS: true,
                 allowTaint: false,
                 backgroundColor: null,
-                scale: 1,
+                scale: 2, // Increase scale for higher resolution
             });
 
             const gridImage = gridCanvas.toDataURL("image/png");

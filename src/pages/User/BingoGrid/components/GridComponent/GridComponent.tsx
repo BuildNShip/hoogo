85d90c1a3d80289c667837387ac7981564d12127
Aswin Asok <aswinasokofficial@gmail.com>
2024-12-05 10:17:52 +0530

    fix(production):placeholder-holder

--- src/pages/User/BingoGrid/components/GridComponent/GridComponent.tsx
+++ src/pages/User/BingoGrid/components/GridComponent/GridComponent.tsx
@@ -348,8 +348,8 @@ const GridComponent: React.FC<BingoGridProps> = ({ cells, setCells, letters }) =
                                         Tell us about your new friend
                                     </label>
                                     <p className={styles.inputFieldDescription}>
-                                        What all did you learn about this person? (Min 10
-                                        characters)
+                                        What are the unique elements you observe in this person
+                                        being a Leader? (Min 10 characters)
                                     </p>
                                     <textarea
                                         id="description"

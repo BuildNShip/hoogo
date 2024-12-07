bacae9d559ca7003eac085db4d50ff1b4188834c
Aswin <aswinasokofficial@gmail.com>
2024-12-07 16:43:18 +0530

    Feat(production):navbar-build-fix

--- src/pages/Landing/components/Navbar.tsx
+++ src/pages/Landing/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { Menu, X } from "lucide-react";
+import { Menu } from "lucide-react";
 import { useState } from "react";
 import styles from "./Navbar.module.css";
 
